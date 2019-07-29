072b33ebb318ce42683adfe648865c2b0648a183
Kenneth Phang <bunnyppl@gmail.com>
2019-07-29 23:06:02 +0800

    new

---
+++ src/main/java/sg/edu/nus/StackAnnotation/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.StackAnnotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
